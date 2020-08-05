c87380d2979c40f6d5b14995b6b3431be1be0e17
Oscar <avelarclaros@gmail.com>
2020-08-05 10:32:47 -0600

    Primer Commit

---
+++ routes/proyectos.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const proyectoController = require('../controllers/proyectoController');
+const auth = require('../middleware/auth');
+const { check } = require('express-validator');
+
+//crea proyectos
+//api/proyectos
+router.post('/',
+	auth,
+	[
+		check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+	],
+	proyectoController.crearProyecto
+);
+
+//obtener todos los proyectos
+router.get('/',
+	auth,
+	proyectoController.obtenerProyectos
+);
+
+// Actualizar Poryecto via ID
+router.put('/:id',
+	auth,
+	[
+		check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+	],
+	proyectoController.actualizarProyecto
+);
+
+//Eliminar proyecto
+router.delete('/:id',
+	auth,
+	proyectoController.eliminarProyecto
+);
+
+module.exports = router;
